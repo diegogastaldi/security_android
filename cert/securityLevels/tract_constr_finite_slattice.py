97129e1a774232b36ad86446748ce4c53a1064a3
diegogastaldi <gastaldidiego@hotmail.com>
2015-07-27 17:29:44 -0300

    tract const finite slattice: add file

---
+++ cert/securityLevels/tract_constr_finite_slattice.py
@@ -0,0 +1,4 @@
+
+def _tract_const_finite_semilattice():
+#    	1: Entrada: Un conjunto finito C = {ti <= Ai} de definite inequalities sobre (L,F),
+#          F = {f: L(con alguna aridad) -> L} con cada f monotona, L reticulo finito, Var(C)=Vm para algun m
